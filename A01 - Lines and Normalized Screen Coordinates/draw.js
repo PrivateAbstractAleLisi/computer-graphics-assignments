8c81185576cc9bb7dab50f6349a921a6349da3e1
ale lisi <10621058@mail.polimi.it>
2021-02-28 09:22:22 +0100

    first assignment

--- A01 - Lines and Normalized Screen Coordinates/draw.js
+++ A01 - Lines and Normalized Screen Coordinates/draw.js
@@ -2,10 +2,6 @@ function draw() {
 	// line(x1,y1, x2,y2)
 	// draws a line from a point at Normalized screen coordinates x1,y1 to Normalized screen coordinates x2,y2
 
-	// Here there are a few random lines, you will have to replace with your code
-	//line(0.3, 0.3,-0.4,-0.4);
-	//line(0.4,-0.4,-0.4, 0.4);
-
 	/* HORIZONTAL */
 	line(-0.5, 0.3, 0.3, 0.3);
 	line(-0.5, -0.3, 0.3, -0.3);
@@ -13,10 +9,23 @@ function draw() {
 	/* VERTICAL */
 	line(-0.5, 0.3, -0.5, -0.3);
 
-	/* CIRCLE */
-	for(i = 0; i <= 64; i++) {
-		//y = 1.6*Math.random() - 0.5;
-		//line(0.4, y,-0.4, y);
+
+	/* CIRCLE - approx. 64  */
+	for(i = 90; i > -90; i--) {
+
+		
+		sin = Math.sin(i * Math.PI / 180)
+		cos =  Math.cos(i * Math.PI / 180)
+
+		sin2 = Math.sin( (i-1) * Math.PI / 180)
+		cos2 = Math.cos( (i-1) * Math.PI / 180)
+
+		x = 0.3 
+		y = 0 
+
+
+		line(x + 0.3*cos, y + 0.3*sin, x + 0.3*cos2 , y + 0.3*sin2)
+
 	}
 
 }
