1c02c05ccd20a3516d942dea277ce37fd3716b98
ale lisi <10621058@mail.polimi.it>
2021-05-12 19:55:06 +0200

    a10 done - da commentare indici per esame

--- A13 - Smooth objects/A13 - Smooth objects/models.js
+++ A13 - Smooth objects/A13 - Smooth objects/models.js
@@ -1,22 +1,22 @@
 function crossProduct(av, bv) {
 
-	res = [
+    res = [
 
-	  av[1] * bv[2] - av[2] * bv[1],
-      av[2] * bv[0] - av[0] * bv[2],
-      av[0] * bv[1] - av[1] * bv[0]
+        av[1] * bv[2] - av[2] * bv[1],
+        av[2] * bv[0] - av[0] * bv[2],
+        av[0] * bv[1] - av[1] * bv[0]
 
-	]
+    ]
 
     return res
-    
-  }
 
- function normalizeVector(vec) {
+}
 
- 	magnitude = Math.sqrt(Math.pow(vec[0],2 ) + Math.pow(vec[1],2 ) + Math.pow(vec[2],2 ))
- 	return [ (vec[0]) / magnitude, Math.abs(vec[1])/magnitude, (vec[2])/magnitude]
- }
+function normalizeVector(vec) {
+
+    magnitude = Math.sqrt(Math.pow(vec[0], 2) + Math.pow(vec[1], 2) + Math.pow(vec[2], 2))
+    return [(vec[0]) / magnitude, Math.abs(vec[1]) / magnitude, (vec[2]) / magnitude]
+}
 
 //from x in a,b to y in c,d
 function mapToRange(X, A, B, C, D) {
@@ -50,82 +50,78 @@ function toTris(vertices, granularity) {
 }
 
 
-
 function buildGeometry() {
 
-	//===========================================================================//
-	// Draws a pyramid --- Already done, just for inspiration
-	var vert1 = [[0.0,1.0,0.0, 0.0, 0.4472,-0.8944],[ 1.0,-1.0,-1.0, 0.0, 0.4472,-0.8944],[-1.0,-1.0,-1.0, 0.0, 0.4472,-0.8944],
-				 [0.0,1.0,0.0, 0.8944, 0.4472,0.0],[ 1.0,-1.0, 1.0, 0.8944, 0.4472,0.0],[ 1.0,-1.0,-1.0, 0.8944, 0.4472,0.0], 
-				 [0.0,1.0,0.0, 0.0, 0.4472,0.8944],[-1.0,-1.0, 1.0, 0.0, 0.4472,0.8944],[ 1.0,-1.0, 1.0, 0.0, 0.4472,0.8944], 
-				 [0.0,1.0,0.0, -0.8944, 0.4472,0.0],[-1.0,-1.0,-1.0, -0.8944, 0.4472,0.0],[-1.0,-1.0, 1.0, -0.8944, 0.4472,0.0], 
-				 [-1.0,-1.0,-1.0, 0.0,-1.0,0.0],[1.0,-1.0,-1.0, 0.0,-1.0,0.0], [1.0,-1.0,1.0, 0.0,-1.0,0.0], [-1.0,-1.0,1.0, 0.0,-1.0,0.0],
-				];
-	var ind1 = [0, 1, 2,  3, 4, 5,  6, 7, 8,  9, 10, 11,  12, 13, 14,  12, 14, 15];
-	var color1 = [0.0, 0.0, 1.0];
-	addMesh(vert1, ind1, color1);
-	
-
-	//===========================================================================//
-
-	// Draws a cube -- To do for the assignment.    //PLANE:
-	var vert2 = 	[[-1.0,-1.0,0.0, 0.0, 0.0,1.0], //Z=0
-	 				[1.0,-1.0,0.0, 0.0, 0.0,1.0],   
-	 			 	[1.0,1.0,0.0, 0.0, 0.0,1.0], 
-	  				[-1.0,1.0,0.0, 0.0, 0.0,1.0],    // 3
-
-	  				[-1.0,	-1.0,	0.0, 0.0, -1.0,	0.0], //Y=-1   // 4
-	 				[1.0,	-1.0,	0.0, 0.0, -1.0,	0.0],
-	 			 	[-1.0,	-1.0,	-2.0, 0.0, -1.0,	0.0], 
-	  				[1.0,	-1.0,	-2.0, 0.0, -1.0,	0.0], 	// 7
-
-	  				[-1.0,	1.0,	0.0, 0.0, 1.0,	0.0], //Y=+1   //8
-	 				[1.0,	1.0,	0.0, 0.0, 1.0,	0.0],
-	 			 	[-1.0,	1.0,	-2.0, 0.0, 1.0,	0.0],  
-	  				[1.0,	1.0,	-2.0, 0.0, 1.0,	0.0],   // 11
-
-	  				[-1.0,	1.0,	0.0, 	-1.0, 0.0,	0.0], //Y=-1   //12
-	 				[-1.0,	-1.0,	0.0, 	-1.0, 0.0,	0.0],
-	 			 	[-1.0,	1.0,	-2.0, 	-1.0, 0.0,	0.0], 
-	  				[-1.0,	-1.0,	-2.0,   -1.0, 0.0,	0.0],    //15
-
-	  				[1.0,	1.0,	0.0, 	1.0, 0.0,	0.0], //Y=-1    //16
-	 				[1.0,	-1.0,	0.0, 	1.0, 0.0,	0.0],
-	 			 	[1.0,	1.0,	-2.0, 	1.0, 0.0,	0.0], 
-	  				[1.0,	-1.0,	-2.0,   1.0, 0.0,	0.0],     //19
-
-	  				[-1.0,-1.0,-2.0, 0.0, 0.0,-1.0], //Z=0
-	 				[1.0,-1.0,-2.0, 0.0, 0.0,-1.0],   
-	 			 	[1.0,1.0,-2.0, 0.0, 0.0,-1.0], 
-	  				[-1.0,1.0,-2.0, 0.0, 0.0,-1.0] 
+    //===========================================================================//
+    // Draws a pyramid --- Already done, just for inspiration
+    var vert1 = [[0.0, 1.0, 0.0, 0.0, 0.4472, -0.8944], [1.0, -1.0, -1.0, 0.0, 0.4472, -0.8944], [-1.0, -1.0, -1.0, 0.0, 0.4472, -0.8944],
+        [0.0, 1.0, 0.0, 0.8944, 0.4472, 0.0], [1.0, -1.0, 1.0, 0.8944, 0.4472, 0.0], [1.0, -1.0, -1.0, 0.8944, 0.4472, 0.0],
+        [0.0, 1.0, 0.0, 0.0, 0.4472, 0.8944], [-1.0, -1.0, 1.0, 0.0, 0.4472, 0.8944], [1.0, -1.0, 1.0, 0.0, 0.4472, 0.8944],
+        [0.0, 1.0, 0.0, -0.8944, 0.4472, 0.0], [-1.0, -1.0, -1.0, -0.8944, 0.4472, 0.0], [-1.0, -1.0, 1.0, -0.8944, 0.4472, 0.0],
+        [-1.0, -1.0, -1.0, 0.0, -1.0, 0.0], [1.0, -1.0, -1.0, 0.0, -1.0, 0.0], [1.0, -1.0, 1.0, 0.0, -1.0, 0.0], [-1.0, -1.0, 1.0, 0.0, -1.0, 0.0],
+    ];
+    var ind1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 12, 14, 15];
+    var color1 = [0.0, 0.0, 1.0];
+    addMesh(vert1, ind1, color1);
 
 
-	  		];
-	var ind2 = [0, 1, 2,  0, 2, 3,	
+    //===========================================================================//
 
-				7,5,4, 4, 6, 7, 
+    // Draws a cube -- To do for the assignment.    //PLANE:
+    var vert2 = [[-1.0, -1.0, 0.0, 0.0, 0.0, 1.0], //Z=0
+        [1.0, -1.0, 0.0, 0.0, 0.0, 1.0],
+        [1.0, 1.0, 0.0, 0.0, 0.0, 1.0],
+        [-1.0, 1.0, 0.0, 0.0, 0.0, 1.0],    // 3
 
-				8,9,10,   11,10,9,
+        [-1.0, -1.0, 0.0, 0.0, -1.0, 0.0], //Y=-1   // 4
+        [1.0, -1.0, 0.0, 0.0, -1.0, 0.0],
+        [-1.0, -1.0, -2.0, 0.0, -1.0, 0.0],
+        [1.0, -1.0, -2.0, 0.0, -1.0, 0.0], 	// 7
 
-				14, 13, 12, 13, 14, 15,
+        [-1.0, 1.0, 0.0, 0.0, 1.0, 0.0], //Y=+1   //8
+        [1.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+        [-1.0, 1.0, -2.0, 0.0, 1.0, 0.0],
+        [1.0, 1.0, -2.0, 0.0, 1.0, 0.0],   // 11
 
-				 16, 17, 18,  19, 18, 17,
+        [-1.0, 1.0, 0.0, -1.0, 0.0, 0.0], //Y=-1   //12
+        [-1.0, -1.0, 0.0, -1.0, 0.0, 0.0],
+        [-1.0, 1.0, -2.0, -1.0, 0.0, 0.0],
+        [-1.0, -1.0, -2.0, -1.0, 0.0, 0.0],    //15
 
-				 22, 21, 20,  23, 22, 20];
-	var color2 = [0.0, 1.0, 1.0];
+        [1.0, 1.0, 0.0, 1.0, 0.0, 0.0], //Y=-1    //16
+        [1.0, -1.0, 0.0, 1.0, 0.0, 0.0],
+        [1.0, 1.0, -2.0, 1.0, 0.0, 0.0],
+        [1.0, -1.0, -2.0, 1.0, 0.0, 0.0],     //19
 
-	addMesh(vert2, ind2, color2);
-	
+        [-1.0, -1.0, -2.0, 0.0, 0.0, -1.0], //Z=0
+        [1.0, -1.0, -2.0, 0.0, 0.0, -1.0],
+        [1.0, 1.0, -2.0, 0.0, 0.0, -1.0],
+        [-1.0, 1.0, -2.0, 0.0, 0.0, -1.0]
 
 
+    ];
+    var ind2 = [0, 1, 2, 0, 2, 3,
+
+        7, 5, 4, 4, 6, 7,
+
+        8, 9, 10, 11, 10, 9,
+
+        14, 13, 12, 13, 14, 15,
+
+        16, 17, 18, 19, 18, 17,
+
+        22, 21, 20, 23, 22, 20];
+    var color2 = [0.0, 1.0, 1.0];
+
+    addMesh(vert2, ind2, color2);
 
 
-	//============================== SIN COS FUNCTION ===============================//
+    //============================== SIN COS FUNCTION ===============================//
 
 
-	let granularity = 15;
-	// Draws function y = sin(x) * cos(z) with -3 <= x <= 3 and -3 <= z <= 3 -- To do for the assignment.
-	var vert3 = [];
+    let granularity = 15;
+    // Draws function y = sin(x) * cos(z) with -3 <= x <= 3 and -3 <= z <= 3 -- To do for the assignment.
+    var vert3 = [];
 
     var pos = 0;
     var tri = 0;
@@ -136,84 +132,130 @@ function buildGeometry() {
             var x = mapToRange(i, 0, granularity, -3, 3);
             var z = mapToRange(j, 0, granularity, -3, 3);
 
-			var y = Math.sin((x)) * Math.cos((z))
+            var y = Math.sin((x)) * Math.cos((z))
 
-			let Fx = Math.cos(x) * Math.cos(z)
-			let Fz = Math.sin(x) * (-Math.sin(z))
-			let Fy = -1
+            let Fx = Math.cos(x) * Math.cos(z)
+            let Fz = Math.sin(x) * (-Math.sin(z))
+            let Fy = -1
 
-			let norma = normalizeVector([-Fx, -Fy, -Fz])
-			vert3[pos] = [x, y, z, norma[0],norma[1],norma[2] ];
+            let norma = normalizeVector([-Fx, -Fy, -Fz])
+            vert3[pos] = [x, y, z, norma[0], norma[1], norma[2]];
             pos = pos + 1;
         }
     }
-	
-	var ind3 = toTris(vert3, granularity);
-	var color3 = [0.0, 1.0, 1.0];
-	addMesh(vert3, ind3, color3);
+
+    var ind3 = toTris(vert3, granularity);
+    var color3 = [0.0, 1.0, 1.0];
+    addMesh(vert3, ind3, color3);
 
 
+    // ============================================================================ //
 
 
+    // Draws a Cylinder --- To do for the assignment
 
-	// ============================================================================ //
+    //upper and lower centers
+    var vert4 = [[0.0, 1.0, 0.0, 0.0, 1.0, 0.0]];
+    vert4[145] = [0.0, -1.0, 0.0, 0.0, -1.0, 0.0];
 
+    {
+        for (i = 0; i < 36; i++) {
 
-	// Draws a Cylinder --- To do for the assignment
+            const step = Math.PI * 16.0 * i / 180.0;
+            //upper ring
+            vert4[i + 1] = [Math.sin(step), 1.0, Math.cos(step), 0.0, 1.0, 0.0];
 
-	//upper and lower centers
-	var vert4 = [[0.0, 1.0, 0.0, 0.0, 1.0, 0.0]];
-	vert4[145] = [0.0, -1.0, 0.0, 0.0, -1.0, 0.0];
+            //upper ring - horizontal normals (perpendicular to the last ones)
+            vert4[i + 37] = [Math.sin(step), 1.0, Math.cos(step), Math.sin(step), 0.0, Math.cos(step)];
 
-	{
-		for (i = 0; i < 36; i++) {
-
-			const step = Math.PI * 16.0 * i / 180.0;
-			//upper ring
-			vert4[i + 1] = [Math.sin(step), 1.0, Math.cos(step), 0.0, 1.0, 0.0];
-
-			//upper ring - horizontal normals (perpendicular to the last ones)
-			vert4[i + 37] = [Math.sin(step), 1.0, Math.cos(step), Math.sin(step), 0.0, Math.cos(step)];
-
-			vert4[i + 73] = [Math.sin(step), -1.0, Math.cos(step), Math.sin(step), 0.0, Math.cos(step)];
-			vert4[i + 109] = [Math.sin(step), -1.0, Math.cos(step), 0.0, -1.0, 0.0];
-		}
+            vert4[i + 73] = [Math.sin(step), -1.0, Math.cos(step), Math.sin(step), 0.0, Math.cos(step)];
+            vert4[i + 109] = [Math.sin(step), -1.0, Math.cos(step), 0.0, -1.0, 0.0];
+        }
 
 
-		////// INDICES ////////
-		var ind4 = [];
+        ////// INDICES ////////
+        var ind4 = [];
 
-		j = 0;
-		for (i = 0; i < 36; i++) { //triangle fan for the upper circle
-			ind4[j++] = 0;
-			ind4[j++] = i + 1;
-			ind4[j++] = (i + 1) % 36 + 1;
-		}
+        j = 0;
+        for (i = 0; i < 36; i++) { //triangle fan for the upper circle
+            ind4[j++] = 0;
+            ind4[j++] = i + 1;
+            ind4[j++] = (i + 1) % 36 + 1;
+        }
 
-		for (i = 0; i < 36; i++) { //triangle fan for the lower circle
-			ind4[j++] = 145;
-			ind4[j++] = (i + 1) % 36 + 109;
-			ind4[j++] = i + 109;
-		}
+        for (i = 0; i < 36; i++) { //triangle fan for the lower circle
+            ind4[j++] = 145;
+            ind4[j++] = (i + 1) % 36 + 109;
+            ind4[j++] = i + 109;
+        }
 
-		for (i = 0; i < 36; i++) { //body
-			ind4[j++] = i + 73;
-			ind4[j++] = (i + 1) % 36 + 37;
-			ind4[j++] = i + 37;
+        for (i = 0; i < 36; i++) { //body
+            ind4[j++] = i + 73;
+            ind4[j++] = (i + 1) % 36 + 37;
+            ind4[j++] = i + 37;
 
-			ind4[j++] = (i + 1) % 36 + 37;
-			ind4[j++] = i + 73;
-			ind4[j++] = (i + 1) % 36 + 73;
-		}
-		let color4 = [1.0, 0.45, 0.7];
-		addMesh(vert4, ind4, color4);
-	}
+            ind4[j++] = (i + 1) % 36 + 37;
+            ind4[j++] = i + 73;
+            ind4[j++] = (i + 1) % 36 + 73;
+        }
+        let color4 = [1.0, 0.45, 0.7];
+        addMesh(vert4, ind4, color4);
+    }
 
-	// Draws a Sphere --- To do for the assignment.
-	var vert5 = [[-1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,1.0,0.0, 0.0, 0.0,1.0], [-1.0,1.0,0.0, 0.0, 0.0,1.0]];
-	var ind5 = [0, 1, 2,  0, 2, 3];
-	var color5 = [1.0, 0.0, 0.0];
-	addMesh(vert5, ind5, color5);
+    // Draws a Sphere --- To do for the assignment.
+    {
+        var vert5 = []
+        var ind5 = []
+        vert5[0] = [0, 1, 0, 0, 1, 0] //apex
+        ///// Creates vertices
+        cur = 1;
+
+        //generate vertices with a step of '10
+        for (let theta = 10; theta < 180; theta = theta + 10) {
+            for (let phi = 0; phi < 360; phi = phi + 10) {
+                x = Math.sin(phi / 180.0 * Math.PI) * Math.sin(theta / 180.0 * Math.PI);
+                y = Math.cos(theta / 180.0 * Math.PI);
+                z = Math.cos(phi / 180.0 * Math.PI) * Math.sin(theta / 180.0 * Math.PI);
+                vert5[cur++] = [x, y, z, x, y, z];
+            }
+        }
+        let last = cur;
+        vert5[cur++] = [0,-1,0, 0, -1, 0];
+
+        // INDICES //
+        k = 0;
+
+        // Upper Fan
+        for(i = 0; i < 360; i+=10) {
+            ind5[k++] = 0; //the apex
+            ind5[k++] = i/10 + 1; //the left point
+            ind5[k++] = (i/10 + 1) % 36 + 1; //right point, %36 is to "return to zero" to create the last fan
+        }
+
+        //Body
+        for(i = 0; i < 360; i+=10) {
+            for(j = 10; j < 170; j+=10) {
+                ind5[k++] = i/10 + (j/10-1) * 36 + 1;
+                ind5[k++] = i/10 + j/10 * 36 + 1;
+                ind5[k++] = (i/10 + 1) % 36 + (j/10-1) * 36 + 1;
+
+                ind5[k++] = (i/10 + 1) % 36 + (j/10-1) * 36 + 1;
+                ind5[k++] = i/10 + j/10 * 36 + 1;
+                ind5[k++] = (i/10 + 1) % 36 + j/10 * 36 + 1;
+            }
+        }
+
+        // Lower Fan
+        for(i = 0; i < 360; i+=10) {
+            ind5[k++] = last;
+            ind5[k++] = (i/10 + 1) % 36 + 541;
+            ind5[k++] = i/10 + 541;
+        }
+
+    }
+
+    let color5 = [0.3, 0.15, 0.25];
+    addMesh(vert5, ind5, color5);
 
 }
 
